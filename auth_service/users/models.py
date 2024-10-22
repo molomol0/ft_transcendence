2e8a0bd86bbc31c144f159d8d75f7c13a455876d
Bilel MOUDACHE <bmoudach@paul-f4Ar5s10.clusters.42paris.fr>
2024-10-22 11:36:23 +0200

    init

--- auth_service/users/models.py
+++
@@ -1,7 +0,0 @@
-# users/models.py
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    display_name = models.CharField(max_length=30, unique=True)  # Contrainte unique
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)  # Permet les valeurs nulles
