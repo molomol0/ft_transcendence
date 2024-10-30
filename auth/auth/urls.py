6428faa157a3652f2dad69dfe4a6279a8c419d80
bilel <bilel.moudache@proton.me>
2024-10-30 20:07:17 +0100

    store files

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views.auth import token ,signup,logout
+from .views.token_management import refresh_token, validate_token
+from .views.email_verification import email_verification,reset_password_confirm
+from .views.password_management import reset_password_request, change_password
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -24,21 +27,21 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # API Authentication endpoints
-    path('api/auth/login/', views.token, name='login'),
-    path('api/auth/signup/', views.signup, name='signup'),
-    path('api/auth/email_verification/<str:uidb64>/<str:token>/', views.email_verification, name='email_verification'),
+    path('api/auth/login/', token, name='login'),
+    path('api/auth/signup/', signup, name='signup'),
+    path('api/auth/email_verification/<str:uidb64>/<str:token>/', email_verification, name='email_verification'),
     
     # API Token endpoint
-    path('api/auth/token/refresh/', views.refresh_token, name='token_refresh'),
-    path('api/auth/token/validate/', views.validate_token, name='token_validate'),
+    path('api/auth/token/refresh/', refresh_token, name='token_refresh'),
+    path('api/auth/token/validate/', validate_token, name='token_validate'),
     
     # API Password reset endpoints
-    path('api/auth/password/reset/', views.reset_password_request, name='password_reset_request'),
-    path('api/auth/password/reset/confirm/<str:uidb64>/<str:token>/', views.reset_password_confirm, name='password_reset_confirm'),
-    path('api/auth/password/change/', views.change_password, name='change_password'),
+    path('api/auth/password/reset/', reset_password_request, name='password_reset_request'),
+    path('api/auth/password/reset/confirm/<str:uidb64>/<str:token>/', reset_password_confirm, name='password_reset_confirm'),
+    path('api/auth/password/change/', change_password, name='change_password'),
 
     # API Logout endpoint
-    path('api/auth/logout/', views.logout, name='logout'),
+    path('api/auth/logout/', logout, name='logout'),
 
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
