afc95b369152c3b8d70a86cb0c3878b45a363745
bilel <bilel.moudache@proton.me>
2024-10-30 19:17:48 +0100

    fix docker : entrypoint migration issu

--- auth/docker/entrypoint_dev.sh
+++ auth/docker/entrypoint_dev.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 # Exécute les migrations
+echo "Exécution des migrations..."
 python manage.py migrate
 
 # Crée un superutilisateur si aucun n'existe
 echo "Création du superutilisateur..."
 python manage.py shell << END
 from django.contrib.auth import get_user_model
+from django.core.management import call_command
 
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
@@ -17,4 +19,5 @@ else:
 END
 
 # Démarre le serveur de développement
+echo "Démarrage du serveur de développement..."
 exec python manage.py runserver 0.0.0.0:8000
--- auth/docker/entrypoint_prod.sh
+++ auth/docker/entrypoint_prod.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
 # Exécute les migrations
+echo "Exécution des migrations..."
 python manage.py migrate
 
-# Démarre le serveur de développement
+# Crée un superutilisateur si aucun n'existe
+echo "Création du superutilisateur..."
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    print("Superutilisateur créé avec succès !")
+else:
+    print("Le superutilisateur existe déjà.")
+END
+
+# Démarre le serveur de production avec Gunicorn
+echo "Démarrage du serveur de production..."
 exec gunicorn --bind 0.0.0.0:8000 auth.wsgi:application
