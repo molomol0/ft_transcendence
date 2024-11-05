b8d39c1cfe89b6c6114ef6fa3dae905d5cc1af2e
SefirOutin <1stalias.stove861@passinbox.com>
2024-11-05 19:17:42 +0100

    more secure delete

--- media-service/media/views/ImageDelete.py
+++ media-service/media/views/ImageDelete.py
@@ -6,12 +6,33 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
 from ..models import UserProfileImage
+from django.core.exceptions import ObjectDoesNotExist
 
 @api_view(['DELETE'])
 @authorize_user
 def	ImageDelete(request):
     username = getattr(request, 'username')
-    user = User.objects.get(username=username)
-    os.remove(UserProfileImage.objects.get(user=user).image.path)
-    UserProfileImage.objects.get(user=user).delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    try:
+        user = User.objects.get(username=username)
+        profileImage = UserProfileImage.objects.get(user=user)
+        
+        if not os.path.exists(profileImage.image.path):
+            return Response(
+                {'error': 'imag not found'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        os.remove(profileImage.image.path)
+        profileImage.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    except ObjectDoesNotExist:
+        return Response(
+            {'error': 'User does not exists'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    except UserProfileImage.DoesNotExist:
+        return Response(
+            {'error': 'image not found'},
+            status=status.HTTP_404_NOT_FOUND
+        )
