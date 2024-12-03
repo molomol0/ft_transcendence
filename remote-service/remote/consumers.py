efa03fdbb303ed361ba4bcb122b98b1a5fcd72df
SefirOutin <1stalias.stove861@passinbox.com>
2024-12-03 20:37:20 +0100

    ball sync server side, can host multiple games

--- remote-service/remote/consumers.py
+++ remote-service/remote/consumers.py
@@ -1,18 +1,37 @@
 import json
-import aioredis
 from django.conf import settings
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from .models import Game, Player
+import asyncio
 
 host, port = settings.CHANNEL_LAYERS['default']['CONFIG']['hosts'][0]
 REDIS_URL = f"redis://{host}:{port}"
 
 games = {}
+games_loop_task = None
+
+async def games_loop():
+	while True:
+		for game in games.values():
+			if game.status:
+				# print(f'active: {game.group_name}')
+				await game.update()
+		await asyncio.sleep(0.016)  # Adjust the sleep time as needed
+
+async def games_send_data_loop():
+	while True:
+		for game in games.values():
+			if game.status:
+				# print(f'active: {game.group_name}')
+				await game.update()
+		await asyncio.sleep(0.5)  # Adjust the sleep time as needed
+
 
 class PongConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
+		global games_loop_task
 		self.room_name = self.scope['url_route']['kwargs']['room_name']
 		self.group_name = f'pong_{self.room_name}'
 		print(f"Connecting to group: {self.group_name}")
@@ -26,29 +45,18 @@ class PongConsumer(AsyncWebsocketConsumer):
 		if self.group_name not in games:
 			games[self.group_name] = Game(self.group_name)
 			self.role = 0
-			# games[self.group_name].add_player(Player(self.role))
 		else :
 			self.role = 1
-		games[self.group_name].add_player(Player(self.role))
-
-		# client_count = await self.get_client_count()
-		# if client_count >= 2:
-		# 	await self.close()
-		# 	return
 		
-		# await self.increment_client_count()
+		if games_loop_task is None:
+			games_loop_task = asyncio.create_task(games_loop())
+			print("Started games loop task")
+		# print(f'glob var game: {games_loop_task}')
 
-		# if not client_count:
-		# 	self.role = 1
-		# else:
-		# 	self.role = 2
-		# await self.send(json.dumps({
-		# 	'event': 'assign_role',
-		# 	'data': f'player{self.role}'
-		# }))
-		# if self.role == 2:
-		# 	await self.channel_layer.group_send(
-		# 		self.group_name,{'type': 'game_started'})
+		games[self.group_name].add_player(Player(self.role))
+		if len(games[self.group_name].players) == 2:
+			await games[self.group_name].start()
+
 
 	async def disconnect(self, close_code):
 		games[self.group_name].remove_player(self.role)
@@ -64,7 +72,6 @@ class PongConsumer(AsyncWebsocketConsumer):
 		await self.channel_layer.group_discard(
 			self.group_name,self.channel_name)
 		
-		# await self.decrement_client_count()
 
 	async def receive(self, text_data):
 		try:
@@ -91,14 +98,21 @@ class PongConsumer(AsyncWebsocketConsumer):
 			await self.send_error(f'Error: {str(e)}')
 
 	async def handle_paddle_moved(self, data):
-		print(f'Paddle moved: {data}')
-		await self.channel_layer.group_send(
-			self.group_name,
-			{
-				'type': 'paddle_moved',
-				'data': data
-			}
-		)
+		print(f'self.role: {self.role} || Paddle moved: {data}')
+		
+		if games[self.group_name].status == False:
+			return
+
+		game = games[self.group_name]
+		game.players[self.role].direction = float(data['direction'])
+		print(f'player direction: {game.players[self.role].direction}')
+		# await self.channel_layer.group_send(
+		# 	self.group_name,
+		# 	{
+		# 		'type': 'paddle_moved',
+		# 		'data': data
+		# 	}
+		# )
 
 	async def handle_start_game(self, data):
 		await self.channel_layer.group_send(
@@ -150,20 +164,36 @@ class PongConsumer(AsyncWebsocketConsumer):
 	async def game_started(self, event):
 		await self.send(text_data=json.dumps({
 			'event': 'game_started',
+			'data': event['data']
 		}))
 
-	# async def player_disconnected(self, event):
-	# 	if self.role == 2:
-	# 		self.role = 1
-	# 	await self.send(json.dumps({
-	# 		'event': 'assign_role',
-	# 		'data': f'player{self.role}'
-	# 	}))
+	async def player_disconnected(self, event):
+		if self.role == 1:
+			self.role = 0
+		await self.assign_role(event)
+
+	async def assign_role(self, event):
+		await self.send(text_data=json.dumps({
+			'event': 'assign_role',
+			'data': 'left' if self.role == 0 else 'right'
+		}))
 
 	async def game_ended(self, event):
 		await self.send(text_data=json.dumps({
 			'event': 'game_ended',
 			# 'data': event['data']
+		}))
+
+	async def game_update(self, event):
+		await self.send(text_data=json.dumps({
+			'event': 'game_update',
+			'data': event['data']
+		}))
+
+	async def game_reset(self, event):
+		await self.send(text_data=json.dumps({
+			'event': 'game_reset',
+			'data': event['data']
 		}))
 
 	async def score_updated(self, event):
@@ -178,29 +208,4 @@ class PongConsumer(AsyncWebsocketConsumer):
 			'data': event['data']
 		}))
 
-	# async def increment_client_count(self):
-	# 	redis = await aioredis.from_url(REDIS_URL)
-	# 	key = f"group:{self.group_name}:count"
-	# 	new_count = await redis.incr(key)
-	# 	print(f"Incremented client count for {key}: {new_count}")
-	# 	await redis.close()
-
-	# async def decrement_client_count(self):
-	# 	redis = await aioredis.from_url(REDIS_URL)
-	# 	key = f"group:{self.group_name}:count"
-	# 	count = await redis.decr(key)
-	# 	print(f"Decremented client count for {key}: {count}")
-	# 	if count <= 0:
-	# 	    # Clean up Redis key if no clients remain
-	# 		await redis.delete(key)
-	# 		print(f"Deleted Redis key for {key} as count is {count}")
-	# 	await redis.close()
-
-	# async def get_client_count(self):
-	# 	# print(f'url: {REDIS_URL}')
-	# 	redis = await aioredis.from_url(REDIS_URL)
-	# 	key = f"group:{self.group_name}:count"
-	# 	count = await redis.get(key)
-	# 	print(f"Current client count for {key}: {count}")
-	# 	await redis.close()
-	# 	return int(count) if count else 0
+# asyncio.create_task(games_loop())
--- remote-service/remote/models.py
+++ remote-service/remote/models.py
@@ -1,3 +1,8 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+import random
+from datetime import datetime
+
 class Player:
 	def __init__ (self, id):
 		# self.id = id
@@ -15,67 +20,125 @@ class Player:
 class Ball:
 	def __init__ (self):
 		self.pos = {'x': 0, 'y': 0}
-		self.velocity = 0.5
+		self.velocity = 1
 		self.direction = {'x': 0.1, 'y': 0.1}
 	
 	def move (self):
-		self.pos['x'] += self.velocity * self.direction
-		self.pos['y'] += self.velocity * self.direction
-	
-	def predict_move(self):
-		return {
-			'x': self.pos['x'] + self.velocity * self.direction['x'],
-			'y': self.pos['y'] + self.velocity * self.direction['y']
-		}
+		self.pos['x'] += self.velocity * self.direction['x']
+		self.pos['y'] += self.velocity * self.direction['y']
 
 
 class Game:
-	def __init__ (self, name):
+	def __init__ (self, group_name):
 		self.time = 0
 		self.ball = Ball()
 		self.players = []
+		self.group_name = group_name
 		self.status = False
+		self.last_update_time = datetime.now()
 
 	def add_player (self, player):
 		self.players.append(player)
 
-	def remove_player (self, index):
-		self.players.pop(index)
+	def remove_player (self, role):
+		print(f'indexxxxxxxxxxxxx: {role}')
+		self.players.pop(role)
 
-	def start (self):
+	async def start (self):
 		if len(self.players) == 2:
 			self.status = True
+		self.reset()
+		self.time = datetime.now()
+		await get_channel_layer().group_send(
+			self.group_name,{'type': 'assign_role'})
+		await get_channel_layer().group_send(
+			self.group_name, {
+				'type': 'game_started',
+				'data': {
+					'ball': {
+						'direction': self.ball.direction,
+						'velocity': self.ball.velocity
+					}
+				# 	'players': {
+				# 		'left': {'pos': self.players[0].pos, 'score': self.players[0].score},
+				# 		'right': {'pos': self.players[1].pos, 'score': self.players[1].score}
+				# 	}
+
+				}
+			}
+		)
 
 	def end (self):
 		self.status = False
 
-	def reset (self):
+	async def reset (self):
 		self.ball.pos = {'x': 0, 'y': 0}
-		self.ball.direction = {'x': 0.1, 'y': 0.1}
+		self.ball.direction = {'x': random.choice([-0.1, 0.1]), 'y': random.choice([-0.1, 0.1])}
 		self.players[0].pos = {'x': -10, 'y': 0}
 		self.players[1].pos = {'x': 10, 'y': 0}
+		await get_channel_layer().group_send(
+			self.group_name, {
+				'type': 'game_reset',
+				'data': {
+					'ball': {
+						'direction': self.ball.direction,
+					}
+				}})
+				# 	'players': {
+				# 		'left': {'pos': self.players[0].pos, 'score': self.players[0].score},
+				# 		'right': {'pos': self.players[1].pos, 'score': self.players[1].score}
+				# 	}
 
-	def update (self):
-		new_move = self.ball.predict_move()
-		self.players[0].move()
-		self.players[1].move()
+		# 		}
+		# 	}
+		# )
 
-		paddle1 = self.players[0].pos
-		paddle2 = self.players[1].pos
-		
-		if new_move['y'] <= -10 or new_move['y'] >= 10:
-			self.ball.direction['y'] *= -1
-		if new_move['x'] <= paddle1['x'] + 0.5 and \
-			new_move['y'] <= paddle1['y'] + 2.5 and \
-			new_move['y'] >= paddle1['y'] - 2.5 or \
-			new_move['x'] >= paddle2['x'] - 0.5 and \
-			new_move['y'] <= paddle2['y'] + 2.5 and \
-			new_move['y'] >= paddle2['y'] - 2.5 :
-			self.ball.direction['x'] *= -1
+	async def update (self):
+		try:
+			# now = datetime.now()
+			# delta_time = (now - self.last_update_time).total_seconds()
+			# self.last_update_time = now
 
-		if new_move['x'] < self.players[0].pos['x']:
-			self.players[1].score += 1
-			self.reset()
-		if new_move['x'] > self.players[1].pos['x']:
-			self.players[0].score += 1
-			self.reset()
+			self.ball.move()
+			self.players[0].move()
+			self.players[1].move()
+			paddle1 = self.players[0].pos
+			paddle2 = self.players[1].pos
+			# print(f'ball: {self.ball.pos}')
+			if self.ball.pos['y'] <= -10 or self.ball.pos['y'] >= 10:
+				self.ball.direction['y'] *= -1
+			
+			if self.ball.pos['x'] <= paddle1['x'] + 0.5 and \
+				self.ball.pos['y'] <= paddle1['y'] + 2.5 and \
+				self.ball.pos['y'] >= paddle1['y'] - 2.5 or \
+				self.ball.pos['x'] >= paddle2['x'] - 0.5 and \
+				self.ball.pos['y'] <= paddle2['y'] + 2.5 and \
+				self.ball.pos['y'] >= paddle2['y'] - 2.5 :
+				self.ball.direction['x'] *= -1
+
+			if self.ball.pos['x'] < self.players[0].pos['x']:
+				self.players[1].score += 1
+				self.reset()
+			if self.ball.pos['x'] > self.players[1].pos['x']:
+				self.players[0].score += 1
+				self.reset()
+
+			self.players[0].direction = 0
+			self.players[1].direction = 0
+			channel = get_channel_layer()
+			if channel is None:
+				print('channel is none')
+			await get_channel_layer().group_send(
+				self.group_name, {
+					'type': 'game_update',
+					'data': {
+						'ball': self.ball.pos,
+						'players': {
+							'left': {'pos': self.players[0].pos, 'score': self.players[0].score},
+							'right': {'pos': self.players[1].pos, 'score': self.players[1].score}
+						}
+					}
+				}
+			)
+		except Exception as e:
+			print(f'Error: {str(e)}')
