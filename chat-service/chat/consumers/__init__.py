c848fe6643dddce4eb333f45f272ddfb2a1e8aff
bilel <bilel.moudache@proton.me>
2024-12-09 19:25:50 +0100

    ok

--- chat-service/chat/consumers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .channels import ChatConsumer
-from .lobby import LobbyConsumer
-
-# connected_users = {}
-
-__all__ = ['ChatConsumer', 'LobbyConsumer']
--- chat-service/chat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from .consumers import LobbyConsumer, ChatConsumer
-
-websocket_urlpatterns = [
-	re_path(r'ws/lobby/', LobbyConsumer.as_asgi()),
-    re_path(r'ws/chat/(?P<chat_type>\w+)/(?P<chat_id>\w+)/$', ChatConsumer.as_asgi()),
-]
