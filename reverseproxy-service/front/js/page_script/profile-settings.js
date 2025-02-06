e68397167ba97dc1c8d7fccc4bdf74d180a091d5
SefirOutin <1stalias.stove861@passinbox.com>
2025-02-06 22:04:33 +0100

    update user infos in settings(username, password, email)

--- reverseproxy-service/front/js/page_script/profile-settings.js
+++ reverseproxy-service/front/js/page_script/profile-settings.js
@@ -125,34 +125,114 @@ document.getElementById('cancelBtnUserMail').addEventListener('click', function 
 function toggleEditForm(fieldId) {
     const form = document.getElementById(`${fieldId}-form`);
     const span = document.getElementById(fieldId);
-    const input = document.getElementById(`${fieldId}-input`);
-    const button = document.getElementById(`editBtn${capitalizeFirstLetter(fieldId)}`);
+    if (fieldId ==! 'userpwrd') {
+		const input = document.getElementById(`${fieldId}-input`);
+	}
+		const button = document.getElementById(`editBtn${capitalizeFirstLetter(fieldId)}`);
     const saveButton = document.getElementById(`saveBtn${capitalizeFirstLetter(fieldId)}`);
 
-	console.log(`BEFORE:: form : ${form.style.display} span : ${span.style.display} input : ${input.style.display} button : ${button.style.display} saveButton : ${saveButton.style.display}`);
+	// console.log(`BEFORE:: form : ${form.style.display} span : ${span.style.display} input : ${input.style.display} button : ${button.style.display} saveButton : ${saveButton.style.display}`);
     if (form.style.display === 'none') {
-        input.placeholder = span.innerText;
+        if (fieldId ==! 'userpwrd') {
+			input.placeholder = span.innerText;
+		}
         form.style.display = 'block';
         span.style.display = 'none';
         button.style.display = 'none';
-        saveButton.style.display = 'inline';
+        // saveButton.style.display = 'inline';
     } else {
         form.style.display = 'none';
         span.style.display = 'inline';
         button.style.display = 'inline';
-        saveButton.style.display = 'none';
+        // saveButton.style.display = 'none';
     }
-	console.log(`AFTER:: form : ${form.style.display} span : ${span.style.display} input : ${input.style.display} button : ${button.style.display} saveButton : ${saveButton.style.display}`);
+	// console.log(`AFTER:: form : ${form.style.display} span : ${span.style.display} input : ${input.style.display} button : ${button.style.display} saveButton : ${saveButton.style.display}`);
     
 }
 
 function saveField(fieldId) {
-    const input = document.getElementById(`${fieldId}-input`);
-    const newValue = input.value;
-    const span = document.getElementById(fieldId);
-    span.innerText = newValue;
+	alert(`saveField(${fieldId})`);
+    // const input = document.getElementById(`${fieldId}-input`);
+    // const newValue = input.value;
+    // const span = document.getElementById(fieldId);
+    // span.innerText = newValue;
+    if (fieldId === 'userMail' || fieldId === 'username') {
+		updateUsernameOrEmail();
+    } else {
+		updatePassword();
+	}
     toggleEditForm(fieldId);
     // Add code here to save the new value to the server if needed
+}
+
+function updatePassword() {
+	console.log('updatePassword()');
+    const oldPassword = document.getElementById('old-userpwrd-input').value;
+    const newPassword = document.getElementById('new-userpwrd-input').value;
+    const accessToken = sessionStorage.getItem('accessToken');
+
+    console.log('Preparing to send request to update password');
+    console.log('old_password:', oldPassword);
+    console.log('new_password:', newPassword);
+
+    fetch('https://localhost:8443/auth/password/update/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        body: JSON.stringify({ old_password: oldPassword, new_password: newPassword })
+    })
+    .then(response => response.json().then(data => ({ status: response.status, body: data })))
+    .then(({ status, body }) => {
+        if (status === 200) {
+            alert('Password updated successfully');
+        } else {
+            alert('Failed to update password: ' + body.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error updating password:', error);
+        alert('An error occurred while updating the password');
+    });
+}
+
+function updateUsernameOrEmail() {
+	const accessToken = sessionStorage.getItem('accessToken');
+	const currentUsername = document.getElementById('username').innerText;
+	const currentEmail = document.getElementById('userMail').innerText;
+	const newUsername = document.getElementById('username-input').value || currentUsername;
+	const newEmail = document.getElementById('userMail-input').value || currentEmail;
+	console.log('Preparing to send request to update user details');
+	console.log('Access Token:', accessToken);
+	console.log('Current Username:', currentUsername);
+	console.log('Current Email:', currentEmail);
+	console.log('New Username:', newUsername);
+	console.log('New Email:', newEmail);
+	fetch('https://localhost:8443/auth/update/', {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + accessToken
+		},
+		body: JSON.stringify({ username: newUsername, email: newEmail })
+	})
+	.then(response => response.json().then(data => ({ status: response.status, body: data })))
+	.then(({ status, body }) => {
+		if (status === 200) {
+			sessionStorage.setItem('username', body.username);
+			document.getElementById('profile-username').innerText = body.username;
+			document.getElementById('username').innerText = body.username;
+			document.getElementById('userMail').innerText = body.email;
+			alert('Changes saved successfully.');
+		} else {
+			alert('Failed to save changes. Please try again.');
+		}
+	})
+	.catch(error => {
+		console.error('Error saving changes:', error);
+		alert('Failed to save changes. Please try again.');
+	});
 }
 
 function capitalizeFirstLetter(string) {
